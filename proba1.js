18aa4c95997a748adadb836efeae1ed3b684c285
MiEvBa <139645836+MiEvBa@users.noreply.github.com>
2023-12-11 00:24:25 +0300

    Add files via upload

---
+++ proba1.js
@@ -0,0 +1,13 @@
+﻿$('#timer').countdown('2024/03/01').on('update.countdown', function(event) {
+
+    var $this = $(this).html(event.strftime(''
+  
+      + '<span>%-w</span> недель'
+  
+      + '<span>%-d</span> дней'
+  
+      ));
+  
+  });
+
+
